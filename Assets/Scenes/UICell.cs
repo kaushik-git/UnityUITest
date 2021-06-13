cba9b382fede8d8721ec2dcca3d46638171e1495
kaushik-banerjee <dev.nmble@gmail.com>
2021-06-13 14:40:24 +0530

    Grid implementation with random cell color and clickable cells.

---
+++ Assets/Scenes/UICell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class UICell : MonoBehaviour
+{
+
+    public Image image;
+    public Text text;
+
+    private int m_cellId;
+    private Color m_colValue;
+
+    public void SetData(int cellId)
+    {
+        m_cellId = cellId;
+        SetToNormal();
+    }
+
+    public void SetSelected(ColorData colData)
+    {
+        image.color = new Color(colData.r / 255f, colData.g / 255f, colData.b / 255f);
+        text.text = (m_cellId + 1).ToString();
+    }
+
+    public void SetToNormal()
+    {
+        image.color = Color.white;
+        text.text = string.Empty;
+    }
+
+    public void OnClicked()
+    {
+        UIGrid.GetInstance.CellClicked(m_cellId);
+    }
+}
---
+++ Assets/Scenes/UIGrid.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGrid : MonoBehaviour
+{
+
+    public int noOfCells = 42;
+    public int aOI = 1;
+    public GameObject cellPrefab;
+    public Transform cellParent;
+
+    private static UIGrid instance;
+    private const int noOfColumns = 5;
+    private CellData[] m_cellData;
+    private UICell[] m_cells;
+    
+
+    public static UIGrid GetInstance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<UIGrid>();
+            return instance;
+        }
+    }
+
+    void Start()
+    {
+       CreateGrid(noOfCells);
+    }
+
+    public void CreateGrid(int cellCount)
+    {
+        m_cellData = new CellData[noOfCells];
+        m_cells = new UICell[noOfCells];
+        for (int i = 0; i < noOfCells; i++)
+        {
+            CellData cData = new CellData(i, GetRandomColorData());
+            m_cellData[i] = cData;
+
+            GameObject go = Instantiate(cellPrefab);
+            m_cells[i] = go.GetComponent<UICell>();
+            m_cells[i].SetData(i);
+
+            go.transform.SetParent(cellParent, false);
+        }
+    }
+
+    public void CellClicked(int cellId)
+    {
+        m_cells[cellId].SetSelected(m_cellData[cellId].colData);
+    }
+    
+
+    public ColorData GetRandomColorData()
+    {
+        return new ColorData(UnityEngine.Random.Range(0, 256),
+                UnityEngine.Random.Range(0, 256),
+                UnityEngine.Random.Range(0, 256));
+    }
+}
+
+public struct ColorData
+{
+    public int r;
+    public int g;
+    public int b;
+
+    public ColorData(int _r, int _g, int _b)
+    {
+        r = _r;
+        g = _g;
+        b = _b;
+    }
+
+    //public override string ToString()
+    //{
+    //    return string.Format("r:{0},g:{1},b:{2}", r, g, b);
+    //}
+}
+
+public struct CellData
+{
+    public int index;
+    public ColorData colData;
+
+    public CellData(int _index, ColorData _colData)
+    {
+        index = _index;
+        colData = _colData;
+    }
+}
+
+public struct int2
+{
+    public int x;
+    public int y;
+
+    public int2(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+}
